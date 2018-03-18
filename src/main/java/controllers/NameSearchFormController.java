9d63bb6b7a82c7cc73fa14cf8b90fb46b62c9711
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2018-03-19 03:30:56 +0530

    50% UI

--- src/main/java/controllers/NameSearchFormController.java
+++ src/main/java/controllers/NameSearchFormController.java
@@ -1,4 +1,56 @@
 package controllers;
 
-public class NameSearchFormController {
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import javafx.animation.Transition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NameSearchFormController implements Initializable {
+
+    @FXML
+    private JFXHamburger sidebarHam;
+
+    @FXML
+    private AnchorPane sidebarPane;
+
+    @FXML
+    private JFXDrawer sidebarDrawer;
+
+    private HamburgerNextArrowBasicTransition transition;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            sidebarPane = FXMLLoader.load(getClass().getResource("/presentation/sidebarContent.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sidebarDrawer.setSidePane(sidebarPane);
+        sidebarDrawer.open();
+        transition = new HamburgerNextArrowBasicTransition(sidebarHam);
+        transition.setRate(-1);
+    }
+
+    public void sidebarHamClicked(MouseEvent mouseEvent) {
+
+        transition.setRate(transition.getRate()*-1);
+        transition.play();
+
+        if (sidebarDrawer.isShown()) {
+            sidebarDrawer.close();
+        } else {
+            sidebarDrawer.open();
+        }
+    }
 }
--- src/main/java/controllers/StartFormController.java
+++ src/main/java/controllers/StartFormController.java
@@ -5,11 +5,18 @@ import com.jfoenix.controls.JFXNodesList;
 import javafx.animation.RotateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -35,13 +42,15 @@ public class StartFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        nameSearchBtn = new JFXButton("Search By Name");
+        Image imageNameBtn = new Image(getClass().getResourceAsStream("/icons/card_employee_badge_identification-512.png"),75,75,false,false);
+        nameSearchBtn = new JFXButton("    Search By Name", new ImageView(imageNameBtn));
         nameSearchBtn.setButtonType(JFXButton.ButtonType.RAISED);
         nameSearchBtn.setFocusTraversable(false);
         nameSearchBtn.getStylesheets().add("css/search_btn.css");
         nameSearchBtn.getStyleClass().add("animated-option-button");
 
-        contentSearchBtn = new JFXButton("Search By Content");
+        Image imageContentBtn = new Image(getClass().getResourceAsStream("/icons/grey_new_seo-37-512.png"),75,75,false,false);
+        contentSearchBtn = new JFXButton("  Search By Content", new ImageView(imageContentBtn));
         contentSearchBtn.setButtonType(JFXButton.ButtonType.RAISED);
         contentSearchBtn.setFocusTraversable(false);
         contentSearchBtn.getStylesheets().add("css/search_btn.css");
@@ -53,8 +62,25 @@ public class StartFormController implements Initializable {
         nodesList.addAnimatedNode(contentSearchBtn);
 
         buttonPane.getChildren().add(nodesList);
-        nodesList.setLayoutY(300);
+        nodesList.setLayoutY(250);
         nodesList.setSpacing(5);
+
+        nameSearchBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, (e)-> {
+
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("/presentation/name_search_form.fxml"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            Stage primaryStage = (Stage) nameSearchBtn.getScene().getWindow();
+            primaryStage.setTitle("Search By Name");
+            Image icon = new Image(getClass().getResourceAsStream("/icons/open-book-icon-32.png"));
+            primaryStage.getIcons().add(icon);
+            primaryStage.resizableProperty().setValue(Boolean.FALSE);
+            primaryStage.setScene(new Scene(root, 800, 600));
+            primaryStage.show();
+        });
 
     }
 
